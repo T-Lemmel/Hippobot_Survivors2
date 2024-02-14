1d887ea842f8732e3ccdc7d179bad321c696eee0
T-Lemmel <lemmeltom@gmail.com>
2024-02-14 16:58:22 +0100

    HAAAAAAAAAAAAAAAAAA

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/t-lemmel/Aquabot_Tom/src/Hippobot_Survivors/aquabot_ekf
-# Build directory: /home/t-lemmel/Aquabot_Tom/src/Hippobot_Survivors/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
